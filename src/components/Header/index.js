683ff1f5946b31069883aefec6de78805e42d559
André Felipe de Sousa Almeida <andre_rdo@hotmail.com>
2020-09-04 23:22:34 -0300

    Primeiro commit, estilização básica

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles';
+
+export default function index() {
+    return (
+        <Container>
+            <h1>Pejotafy</h1>
+        </Container>
+    )
+}
