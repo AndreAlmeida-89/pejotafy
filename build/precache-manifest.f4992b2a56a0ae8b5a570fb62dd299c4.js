6f5974fa42279f0c4095a799381ccbee70202950
André Felipe de Sousa Almeida <andre_rdo@hotmail.com>
2020-09-05 12:53:44 -0300

    criada a pasta build

---
+++ build/precache-manifest.f4992b2a56a0ae8b5a570fb62dd299c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4f32d946c4c7250713674c55b52a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dad4deab1fe5a02fa27",
+    "url": "/static/js/2.18af1767.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18af1767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05831b7d629cb0226c7",
+    "url": "/static/js/main.9c6df491.chunk.js"
+  },
+  {
+    "revision": "ecca74242a7b77cad168",
+    "url": "/static/js/runtime-main.5a19b172.js"
+  }
+]);
